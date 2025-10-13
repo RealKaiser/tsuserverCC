52c590948c8a7ca25aaf3d3ae873d1d9b24d65cc
RealKaiser <kaiserkaisie@gmail.com>
2025-10-13 18:02:04 +0200

    make sure the trusted users file gets modified properly

--- server/client_manager.py
+++ server/client_manager.py
@@ -924,6 +924,7 @@ class ClientManager:
                             break
                     if not match:
                         trusted.append({'DiscordName': self.discord_name})
+                        trusted[-1]['IPIDs'] = trustedIPIDs
                         trusted[-1]['IPIDs'].append({'IPID': client.ipid})
                 with open(trustedfile, 'w', encoding='utf-8') as newfile:
                     yaml.dump(trusted, newfile)
